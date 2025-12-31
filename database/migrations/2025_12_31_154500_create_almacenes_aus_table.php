82ada62dd538a0f83a197d22e18fa3f8cb2fce3e
joseph <rock3t2013@gmail.com>
2025-12-31 12:27:56 -0400

    Actualizar esquema de la tabla almacenes_aus

--- database/migrations/2025_12_31_154500_create_almacenes_aus_table.php
+++ database/migrations/2025_12_31_154500_create_almacenes_aus_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,9 +12,11 @@ return new class extends Migration
     {
         Schema::create('almacenes_aus', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre');
-            $table->string('codigo', 50)->nullable();
-            $table->text('descripcion')->nullable();
+            $table->integer('cantidad');
+            $table->unsignedBigInteger('sede_id')->nullable();
+            $table->unsignedBigInteger('lote_id')->nullable();
+            $table->unsignedBigInteger('hospital_id')->nullable();
+            $table->boolean('status')->default(true);
             $table->timestamps();
         });
     }
